9ba79c701e58e39aa1bd6d993b1c3a3bb6b6ea5a
Dipanshu-verma <vermadipanshu444@gmail.com>
2024-09-30 22:11:09 +0530

    bot intial

--- bot.js
+++ bot.js
@@ -99,7 +99,7 @@ app.get('/', (req, res) => {
 });
 
  
-const PORT = process.env.PORT || 8001;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
